45aa14f65224083179f28f95e49b475c65429e49
Everton Sales <46347115+tomsales92@users.noreply.github.com>
2020-05-20 22:22:02 -0300

    Validação de Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import GobalStyle from './styles/global';
-// import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+// import SignUp from './pages/SignUp';
 
 const App: React.FC = () => {
   return (
     <>
-      <SignUp />
+      <SignIn />
       <GobalStyle />
     </>
   );
